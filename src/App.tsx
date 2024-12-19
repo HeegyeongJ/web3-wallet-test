62f15116196bc5e709ee219b0575fcdc2fbbbc4c
HeegyeongJ <gmlrud0409@gmail.com>
2024-12-19 17:02:56 +0900

    coinbase

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,62 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {AppKitProvider} from "./context";
+import Onboard from '@web3-onboard/core'
+import coinbaseWalletModule from '@web3-onboard/coinbase'
+
+const main = async () => {
+    const coinbaseWalletSdk = coinbaseWalletModule()
+    const chains = [
+        {
+            id: 1,
+            token: 'ETH',
+            label: 'Ethereum Mainnet',
+            rpcUrl: 'https://mainnet.infura.io/v3/${INFURA_ID}'
+        },
+        {
+            id: 42161,
+            token: 'ARB-ETH',
+            label: 'Arbitrum One',
+            rpcUrl: 'https://rpc.ankr.com/arbitrum'
+        },
+        {
+            id: '0xa4ba',
+            token: 'ARB',
+            label: 'Arbitrum Nova',
+            rpcUrl: 'https://nova.arbitrum.io/rpc'
+        },
+        {
+            id: 137,
+            token: 'MATIC',
+            label: 'Matic Mainnet',
+            rpcUrl: 'https://matic-mainnet.chainstacklabs.com'
+        },
+        {
+            id: '0x2105',
+            token: 'ETH',
+            label: 'Base',
+            rpcUrl: 'https://mainnet.base.org'
+        }
+    ]
+    const onboard = Onboard({
+        chains,
+        wallets: [
+            coinbaseWalletSdk
+        ]
+    })
+
+    const connectedWallets = await onboard.connectWallet()
+    console.log(connectedWallets)
+}
 
 
 function App() {
+    useEffect(() => {
+        main()
+    }, []);
   return (
     <div className="App">
       {/* eslint-disable-next-line react/jsx-no-undef */}
-        <AppKitProvider/>
         <header className="App-header">d
             <img src={logo} className="App-logo" alt="logo" />
         <p>
