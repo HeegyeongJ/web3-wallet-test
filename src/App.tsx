6541373af4cc5f49c5ca11a1d375d576e216633e
HeegyeongJ <gmlrud0409@gmail.com>
2024-12-26 15:02:31 +0900

    add wagmi

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,31 @@
 import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Onboard from '@web3-onboard/core'
-import coinbaseWalletModule from '@web3-onboard/coinbase'
-
-const main = async () => {
-    const coinbaseWalletSdk = coinbaseWalletModule()
-    const chains = [
-        {
-            id: 1,
-            token: 'ETH',
-            label: 'Ethereum Mainnet',
-            rpcUrl: 'https://mainnet.infura.io/v3/${INFURA_ID}'
-        },
-        {
-            id: 42161,
-            token: 'ARB-ETH',
-            label: 'Arbitrum One',
-            rpcUrl: 'https://rpc.ankr.com/arbitrum'
-        },
-        {
-            id: '0xa4ba',
-            token: 'ARB',
-            label: 'Arbitrum Nova',
-            rpcUrl: 'https://nova.arbitrum.io/rpc'
-        },
-        {
-            id: 137,
-            token: 'MATIC',
-            label: 'Matic Mainnet',
-            rpcUrl: 'https://matic-mainnet.chainstacklabs.com'
-        },
-        {
-            id: '0x2105',
-            token: 'ETH',
-            label: 'Base',
-            rpcUrl: 'https://mainnet.base.org'
-        }
-    ]
-    const onboard = Onboard({
-        chains,
-        wallets: [
-            coinbaseWalletSdk
-        ]
-    })
-
-    const connectedWallets = await onboard.connectWallet()
-    console.log(connectedWallets)
-}
-
+import {useAccountCenter, useConnectWallet, useWagmiConfig} from "@web3-onboard/react";
+import {signMessage} from "@web3-onboard/wagmi";
+import {WagmiConfig} from "@web3-onboard/core/dist/types";
 
 function App() {
-    useEffect(() => {
-        main()
-    }, []);
-  return (
+    const [{wallet, connecting}, connect] = useConnectWallet()
+        const wagmiConfig = useWagmiConfig() as WagmiConfig
+    console.log('wagmi', wagmiConfig)
+    const signMessages = async () => {
+        try{
+            await signMessage(wagmiConfig, {
+                message: 'heelelldlldl',
+                connector: wallet?.wagmiConnector
+            })
+        }catch (e){
+            console.log('error',e)
+        }
+    }
+
+
+    return (
     <div className="App">
       {/* eslint-disable-next-line react/jsx-no-undef */}
+        <button onClick={() => connect()}>click</button>
+        <button onClick={() => signMessages()}>signing message</button>
         <header className="App-header">d
             <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,77 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {init, Web3OnboardProvider} from "@web3-onboard/react";
+import coinbaseWalletModule from "@web3-onboard/coinbase";
+import wagmi from '@web3-onboard/wagmi'
+import Onboard from '@web3-onboard/core'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const coinbaseWalletSdk = coinbaseWalletModule()
+const INFURA_KEY = '2896ff3d0a1143689424a8341cb75c67'
+const web3Onboard = Onboard({
+    wagmi,
+    wallets:[coinbaseWalletSdk],
+    chains: [
+        {
+            // hex encoded string, eg '0x1' for Ethereum Mainnet
+            id: '0x1',
+            // string indicating chain namespace. Defaults to 'evm' but will allow other chain namespaces in the future
+            namespace: 'evm',
+            // the native token symbol, eg ETH, BNB, MATIC
+            token: 'ETH',
+            // used for display, eg Ethereum Mainnet
+            label: 'Ethereum Mainnet',
+            // used for network requests
+            rpcUrl: `https://mainnet.infura.io/v3/${INFURA_KEY}`
+        },
+        {
+            id: 42161,
+            token: 'ARB-ETH',
+            label: 'Arbitrum One',
+            rpcUrl: 'https://rpc.ankr.com/arbitrum'
+        },
+        {
+            id: '0xa4ba',
+            token: 'ARB',
+            label: 'Arbitrum Nova',
+            rpcUrl: 'https://nova.arbitrum.io/rpc'
+        },
+        {
+            id: '0x89',
+            token: 'MATIC',
+            label: 'Matic Mainnet',
+            rpcUrl: 'https://matic-mainnet.chainstacklabs.com'
+        },
+        {
+            id: '0x2105',
+            token: 'ETH',
+            label: 'Base',
+            rpcUrl: 'https://mainnet.base.org'
+        },
+        {
+            id: '0xa4ec',
+            token: 'ETH',
+            label: 'Celo',
+            rpcUrl: 'https://1rpc.io/celo'
+        },
+        {
+            id: 666666666,
+            token: 'DEGEN',
+            label: 'Degen',
+            rpcUrl: 'https://rpc.degen.tips'
+        }
+    ],
+})
+
 root.render(
   <React.StrictMode>
-    <App />
+      <Web3OnboardProvider web3Onboard={web3Onboard} >
+        <App />
+      </Web3OnboardProvider>
   </React.StrictMode>
 );
 
